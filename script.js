fdf4fccf31123d545c77cc0110f72b0aa2cbd4ec
Tal Zadik <talzadik@Tals-MacBook-Pro.local>
2022-06-22 23:08:03 +0300

    milestone1

---
+++ script.js
@@ -0,0 +1,6 @@
+let sequenceNum = 0;
+let result = 0;
+let sentence = `The Fibonacci of ${sequenceNum} is ${result}`;
+
+let fibtext = document.getElementById("text");
+fibtext.innerHTML = sentence;
