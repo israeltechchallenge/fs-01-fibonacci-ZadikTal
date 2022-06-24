d8fdbc100e00ca1710268436a668e7601d44533b
Tal Zadik <talzadik@Tals-MacBook-Pro.local>
2022-06-24 14:31:42 +0300

    fix problems clean inputField and repetitive error42 message

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let spinnerLoad = document.getElementById("sppinner");
 let inputField = document.querySelector("#sequenceInput");
 const errorDivPlaceholder = document.querySelector("#error-div-placeholder");
 const calcDiv = document.querySelector("#calc-div");
-
+let btn = document.querySelector("#resultBtn");
 
 function fibcalc(sequence) {
   let url = "http://localhost:5050/fibonacci/" + sequence;
@@ -18,27 +18,40 @@ function fibcalc(sequence) {
       spinnerLoad.classList.add("d-none");
     })
     .catch((error) => {
+      cleanUpErrorMessege();
       let errorDiv42 = document.createElement("div");
       errorDiv42.innerHTML = "Server Error: 42 is the meaning of life";
-      errorDiv42.classList.add( "text-danger", "ms-1", "text-opacity-75", "error-box42");
+      errorDiv42.classList.add(
+        "text-danger",
+        "ms-1",
+        "text-opacity-75",
+        "error-box42"
+      );
       errorDiv42.setAttribute("id", "error42");
       calcDiv.appendChild(errorDiv42);
       spinnerLoad.classList.add("d-none");
     });
 }
 
-let btn = document.querySelector("#resultBtn");
-btn.addEventListener("click", function () {
+function cleanUpErrorMessege() {
+  try {
+    inputField.classList.remove("border-danger", "text-danger");
+  } catch {}
+
   try {
     let error50 = document.getElementById("error50");
     error50.parentElement.removeChild(error50);
-  } catch {};
+  } catch {}
 
   try {
     let error42 = document.getElementById("error42");
     error42.parentElement.removeChild(error42);
-  } catch {};
-  
+  } catch {}
+}
+
+btn.addEventListener("click", function () {
+  cleanUpErrorMessege();
+
   document.getElementById("resultNum").innerHTML = "";
   let sequenceInput = document.getElementById("sequenceInput").value;
   spinnerLoad.classList.remove("d-none");
@@ -49,10 +62,15 @@ btn.addEventListener("click", function () {
     let errorDiv = document.createElement("div");
     errorDiv.innerHTML = "Can't be larger than 50";
     errorDiv.setAttribute("id", "error50");
-    errorDiv.classList.add("bg-danger", "border-danger", "text-danger", "bg-opacity-25", "rounded-1", "error-box50");
+    errorDiv.classList.add(
+      "bg-danger",
+      "border-danger",
+      "text-danger",
+      "bg-opacity-25",
+      "rounded-1",
+      "error-box50"
+    );
     errorDivPlaceholder.appendChild(errorDiv);
-
-
   } else {
     if (sequenceInput > 0) {
       fibcalc(sequenceInput);
