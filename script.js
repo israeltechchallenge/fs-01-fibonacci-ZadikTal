7eb98e0ef2177a617d7b5ca4588082a21d11606b
Tal Zadik <talzadik@Tals-MacBook-Pro.local>
2022-06-27 17:26:47 +0300

    milestone6

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ function fibcalc(sequence) {
       throw new Error("Server Error: 42 is the meaning of life");
     })
     .then((data) => {
+      calcList();
       document.getElementById("resultNum").innerHTML = data["result"];
-      spinnerLoad.classList.add("d-none");
     })
     .catch((error) => {
       cleanUp();
@@ -35,7 +35,6 @@ function fibcalc(sequence) {
       errorDiv42.classList.add("text-danger", "ms-1", "text-opacity-75");
       errorDiv42.setAttribute("id", "error42");
       calcDiv.appendChild(errorDiv42);
-      spinnerLoad.classList.add("d-none");
     });
 }
 
@@ -56,12 +55,33 @@ function cleanUp() {
   } catch {}
 }
 
+window.onload = () => calcList();
+function calcList() {
+  let resultBox = document.getElementById("resultsList");
+  resultBox.innerHTML = "";
+  let url = "http://localhost:5050/getFibonacciResults";
+  fetch(url)
+    .then((response) => response.json())
+    .then((oldData) => {
+      spinnerLoad.classList.add("d-none");
+      const arrOfResults = oldData.results.sort(
+        (a, b) => b.createdDate - a.createdDate
+      );
+      for (let i = 0; i < arrOfResults.length; i++) {
+        let resultSentence = `<p class="text-decoration-underline pb-3">The Fibbonaci of <b>${
+          arrOfResults[i].number
+        }</b> is <b>${arrOfResults[i].result}</b>. Calculated at: ${new Date(
+          arrOfResults[i].createdDate
+        )}</p>`;
+        resultBox.innerHTML += resultSentence;
+      }
+    });
+}
+
 btn.addEventListener("click", function () {
   cleanUp();
   let sequenceInput = document.getElementById("sequenceInput").value;
-  spinnerLoad.classList.remove("d-none");
   if (sequenceInput > 50) {
-    spinnerLoad.classList.add("d-none");
     inputField.classList.remove("border-dark", "border-opacity-25");
     inputField.classList.add("border-danger", "text-danger");
     errorDivPlaceholder.appendChild(errorDivOver50);
@@ -69,6 +89,7 @@ btn.addEventListener("click", function () {
     if (sequenceInput > 0) {
       fibcalc(sequenceInput);
     }
-    spinnerLoad.classList.add("d-none");
   }
+  calcList();
+  spinnerLoad.classList.remove("d-none");
 });
